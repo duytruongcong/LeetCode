b71010d74d5aa2af3dfd5940778f65eb5c8bcd83
Duy Truong <truongcongduy.it@gmail.com>
2024-06-14 17:21:37 +0700

    duy working

--- ConsoleAppTestLeetCode/ConsoleAppTestLeetCode/LFUCache.cs
+++ ConsoleAppTestLeetCode/ConsoleAppTestLeetCode/LFUCache.cs
@@ -8,19 +8,38 @@ namespace ConsoleAppTestLeetCode
 {
     public class LFUCache
     {
+        int _capacity;
+        Dictionary<int, int> _cache;
+
         public LFUCache(int capacity)
         {
-
+            _capacity = capacity;
+            _cache = new Dictionary<int, int>(capacity);
         }
 
         public int Get(int key)
         {
-            return 1;
+            if (_cache != null && _cache.ContainsKey(key))
+                return _cache[key];
+            else
+                return -1;
         }
 
         public void Put(int key, int value)
         {
+            _cache.Add(key, value);
 
+            if (_cache.Count == _capacity)
+            {
+
+            }
         }
+    }
+
+    public class Node
+    {
+        public int Key;
+        public int Value;
+        public int Count = 0;
     }
 }
