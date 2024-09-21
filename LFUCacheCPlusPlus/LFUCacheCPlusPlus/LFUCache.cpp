72bf0e2c4ec68de1de79320e493485409ee9e653
ADMIN\Admin <truongcongduy.it@gmail.com>
2024-09-21 12:08:20 +0700

    move code to cpp file

--- LFUCacheCPlusPlus/LFUCacheCPlusPlus/LFUCache.cpp
+++ LFUCacheCPlusPlus/LFUCacheCPlusPlus/LFUCache.cpp
@@ -1,1 +1,42 @@
 #include "LFUCache.h"
+
+class LFUCache
+{
+public:
+	int _capacity;
+	Node* _pCache;
+
+	LFUCache(int capacity)
+	{
+		_capacity = capacity;
+		_pCache = new Node[capacity];
+
+		for (int i = 0; i <= capacity; i++)
+		{
+			_pCache[i] = new Node();
+		}
+	}
+
+	~LFUCache()
+	{
+		delete _pCache;
+		_pCache = nullptr;
+	}
+};
+
+class Node
+{
+public:
+	int Key;
+	int Value;
+	int Count;
+	int Old;
+
+	Node()
+	{
+		Key = -1;
+		Value = -1;
+		Count = 0;
+		Old = 0;
+	}
+};
