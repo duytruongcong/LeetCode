f7fddbc8cc6cef8716e4a2e2b224f2033bab46d6
ADMIN\Admin <truongcongduy.it@gmail.com>
2024-10-27 18:25:25 +0700

    add TwoSumCPP

---
+++ ConsoleAppTestLeetCode/TwoSumCPP/TwoSumCPP.cpp
@@ -0,0 +1,56 @@
+// TwoSumCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+	int number;
+	int nums[10];
+
+	srand(static_cast<unsigned int>(time(0)));
+
+	int numsCount = sizeof(nums) / sizeof(int);
+
+	for (int i = 0; i < numsCount; i++)
+	{
+		nums[i] = rand() % 10;
+	}
+
+	std::cout << "Array is:\n";
+
+	for (int i = 0; i < numsCount; i++)
+	{
+		std::cout << "(" << i << ")" << nums[i] << "->";
+	}
+
+	std::cout << "\nEnter an integer number:\n";
+
+	std::cin >> number;
+
+	for (int i = 0; i < numsCount; i++)
+	{
+		for (int j = i + 1; j < numsCount; j++)
+		{
+			if ((nums[i] + nums[j]) == number)
+			{
+				std::cout << "(" << i << ")" << nums[i] << " | " << "(" << j << ")" << nums[j] << std::endl;
+			}
+		}
+	}
+
+	system("pause");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
