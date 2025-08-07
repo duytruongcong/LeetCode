2fe0b668183ea9616c7269fbb2779d6c9f5a06ad
ADMIN\Admin <truongcongduy.it@gmail.com>
2025-08-07 16:07:32 +0700

    start point

--- ConsoleAppTestLeetCode/TestCode/Main.cpp
+++ ConsoleAppTestLeetCode/TestCode/Main.cpp
@@ -3,41 +3,10 @@
 #include <array>
 using namespace std;
 
-class Entity
-{
-private:
-	int* m_X;
-	int* m_Y;
-	mutable int var;
-public:
-	const int* const GetX() const
-	{
-		var = 2;
-		return m_X;
-	}
-
-	void SetX(int* x)
-	{
-		m_X = x;
-	}
-};
-
-void PrintEntity(const Entity* const e)
-{
-	cout << e->GetX() << endl;
-}
-
-void PrintEntityNew(const Entity& e)
-{
-	cout << e.GetX() << endl;
-}
-
 
 int main()
 {
-	const int MAX_AGE = 90;
-
-	const int const* const a = new int;
+	
 
 	cin.get();
 	return 0;
