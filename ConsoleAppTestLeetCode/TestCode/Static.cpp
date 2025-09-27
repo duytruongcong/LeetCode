5338474be9631bbaeccad554fe9b3fb227af59af
ADMIN\Admin <truongcongduy.it@gmail.com>
2025-09-27 21:17:18 +0700

    add pragma code

--- ConsoleAppTestLeetCode/TestCode/Static.cpp
+++ ConsoleAppTestLeetCode/TestCode/Static.cpp
@@ -1,6 +1,24 @@
-int s_Variable;
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
 
-static void Function()
+struct MyStruct
 {
+	int x, y;
+};
 
+#if false
+int main()
+{
+	MyStruct* a = new MyStruct{ 2,3 };
+	MyStruct* b = a;
+	b->x = 5;
+	cout << a->x << " - " << a->y << endl;
+	cout << b->x << " - " << b->y << endl;
+
+	cin.get();
+	return 0;
 }
+#endif // 0
+
