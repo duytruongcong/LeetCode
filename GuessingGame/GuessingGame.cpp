c7c4c8b15eb54fcdbf5b54f2afd0b16bcf390832
TRUONG CONG DUY <truongcongduy.it@gmail.com>
2026-05-08 09:38:53 +0700

    duy upgrade game , add level

--- GuessingGame/GuessingGame.cpp
+++ GuessingGame/GuessingGame.cpp
@@ -1,32 +1,74 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <tuple>
+
+enum Level
+{
+    LOW,
+    MEDIUM,
+    HIGH
+};
+
+std::tuple<int, int> CreateValues(int level)
+{
+    int randomNum;
+    int maxValue;
+    srand(time(NULL));
+
+    switch (level)
+    {
+
+    case LOW:
+        randomNum = (rand() % 20) + 1; // from 01 to 20
+        maxValue = 20;
+        break;
+        
+    case MEDIUM:
+        randomNum = (rand() % 50) + 1; // from 01 to 50
+        maxValue = 50;
+        break;
+
+    case HIGH:
+    default:
+        randomNum = (rand() % 100) + 1; // from 01 to 100
+        maxValue = 100;
+        break;
+    }
+
+    return std::make_tuple(randomNum, maxValue);
+}
 
 int main()
 {
     int randomNum;
     int guessNum;
     int tries;
+    int intLevel;
+    int maxValue;
     bool isContinue = true;
 
     std::cout << "********GUESSING NUMBER FUNNY GAME********" << std::endl;
 
     do
     {
-        srand(time(NULL));
-        randomNum = (rand() % 100) + 1;
+        // choose level
+        std::cout << "Which level do you quan to play? \n 0 - LOW \n 1 - MEDIUM \n 2 - HIGH" << std::endl;
+        std::cin >> intLevel;
+
+        auto [randomNum, maxValue] = CreateValues(intLevel);
 
         do
         {
-            std::cout << "Please guess the number from 01 to 100 : " << std::endl;
+            std::cout << "Please guess the number from 01 to " << maxValue << " : " << std::endl;
 
             std::cin >> guessNum;
 
             tries++;
 
-            if (guessNum <= 0 || guessNum > 100)
+            if (guessNum <= 0 || guessNum > maxValue)
             {
-                std::cout << "The number must be from 01 to 100, try again!" << std::endl;
+                std::cout << "The number must be from 01 to " << maxValue << ", try again!" << std::endl;
                 continue;
             }
 
