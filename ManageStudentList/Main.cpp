81cad2b50b4279c6ddf2ed13fbb4c43bb73c7686
TRUONG CONG DUY <truongcongduy.it@gmail.com>
2026-05-10 23:42:38 +0700

    test load data

--- ManageStudentList/Main.cpp
+++ ManageStudentList/Main.cpp
@@ -8,19 +8,20 @@
 int main()
 {
     Manager Manager;
-    Manager.Add( "Duy", 36, 8);
-    Manager.Add( "Trang", 30, 9);
-    Manager.Add( "Tam", 20, 7);
-    Manager.Add( "Minh", 40, 5);
-    Manager.Add( "Lan", 40, 2);
-    Manager.Add( "Phuong", 26, 7);
-    Manager.View();
+    // Manager.Add( "Duy", 36, 8);
+    // Manager.Add( "Trang", 30, 9);
+    // Manager.Add( "Tam", 20, 7);
+    // Manager.Add( "Minh", 40, 5);
+    // Manager.Add( "Lan", 40, 2);
+    // Manager.Add( "Phuong", 26, 7);
+    // Manager.View();
 
-    Manager.Save("students.csv");
+    //Manager.Save("students.csv");
+    Manager.Load("students.csv");
     //Manager.EditById( 4, "Minh heo", 40, 5);
 
-    //std::cout << "---------------" << std::endl;
-    //Manager.View();
+    std::cout << "---------------" << std::endl;
+    Manager.View();
 
 
     return 0;
